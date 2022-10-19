f477723ab6e9faee8754b5f83cb212d13d636cf2
william-nguyen128 <nguyenhongquan2k@gmail.com>
2022-10-19 15:53:35 +0700

    feat(images.js): Clean up

--- src/images/images.js
+++ src/images/images.js
@@ -4,12 +4,4 @@ import glassImg from "./glass.png";
 import logImg from "./log.jpg";
 import woodImg from "./wood.png";
 
-const images = {
-  dirtImg,
-  grassImg,
-  glassImg,
-  woodImg,
-  logImg,
-};
-
-export default images;
+export { dirtImg, grassImg, glassImg, woodImg, logImg };
