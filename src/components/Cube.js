0f219dd81ba5de0c65d355d3ac6557c870968308
william-nguyen128 <nguyenhongquan2k@gmail.com>
2022-10-19 22:12:58 +0700

    feat(components/Cube.js): Add cube to another cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,6 +1,7 @@
 import { useBox } from "@react-three/cannon";
 
 import * as textures from "../images/textures";
+import { useStore } from "../hooks/useStore";
 
 export const Cube = ({ position, texture }) => {
   const [ref] = useBox(() => ({
@@ -8,9 +9,48 @@ export const Cube = ({ position, texture }) => {
     position,
   }));
   const activeTexture = textures[texture + "Texture"];
+  const [addCube, removeCube] = useStore((state) => [
+    state.addCube,
+    state.removeCube,
+  ]);
 
   return (
-    <mesh ref={ref}>
+    <mesh
+      ref={ref}
+      onClick={(e) => {
+        e.stopPropagation();
+        const clickedFace = Math.floor(e.faceIndex / 2);
+        const { x, y, z } = ref.current.position;
+        if (e.altKey) {
+          removeCube(x, y, z);
+          return;
+        }
+        if (clickedFace === 0) {
+          addCube(x + 1, y, z);
+          return;
+        }
+        if (clickedFace === 1) {
+          addCube(x - 1, y, z);
+          return;
+        }
+        if (clickedFace === 2) {
+          addCube(x, y + 1, z);
+          return;
+        }
+        if (clickedFace === 3) {
+          addCube(x, y - 1, z);
+          return;
+        }
+        if (clickedFace === 4) {
+          addCube(x, y, z + 1);
+          return;
+        }
+        if (clickedFace === 5) {
+          addCube(x, y, z - 1);
+          return;
+        }
+      }}
+    >
       <boxBufferGeometry attach="geometry" />
       <meshStandardMaterial map={activeTexture} attach="material" />
     </mesh>
